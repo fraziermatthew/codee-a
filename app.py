5260532e260c4b81a8175d5cf0648ec7e1156e29
fraziermatthew <fraziermatthew@hotmail.com>
2023-11-14 10:38:01 -0500

    feat: add feedback

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import datetime
 import time
+
+import pytz
+import sqlite3
+from uuid import uuid4
+
 import streamlit as st
 from utils import load_chain
+from modal import Modal
+
+
+# Create a user_id for the session
+user_id = uuid4()
+
+# Initialize disabled for form_submit_button to False
+if "modal-opened" not in st.session_state:
+    st.session_state[f'modal-opened'] = False
 
 # Custom image for the app icon and the assistant's avatar
 csp_logo = 'https://apcentral.collegeboard.org/media/images/desktop/ap-computer-science-principles-192.png'
@@ -17,7 +32,9 @@ st.set_page_config(
 
 with st.expander("ℹ️ Disclaimer"):
     st.caption(
-        "We appreciate your engagement! Please note, this is research purposes only. Thank you for your understanding."
+        f"""We appreciate your engagement! Please note, this is research purposes only. Thank you for your understanding.
+        Your user-id is {user_id}. Be sure to add this to the survey.
+        """
     )
     
 # Initialize LLM chain in session_state
@@ -28,7 +45,7 @@ if 'chain' not in st.session_state:
 if 'messages' not in st.session_state:
     # Start with first message from assistant
     st.session_state['messages'] = [{"role": "assistant", 
-                                  "content": "Hi student! I'm an intelligent AI for Computer Science Principles. How can I help you today?"}]
+                                  "content": f"Hi student! I'm an intelligent AI for Computer Science Principles. How can I help you today?"}]
 
 # Display chat messages from history on app rerun
 # Custom avatar for the assistant, default avatar for user
@@ -44,12 +61,14 @@ for message in st.session_state.messages:
 if query := st.chat_input("Let's chat"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": query})
+    
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(query)
 
     with st.chat_message("assistant", avatar=csp_logo):
         message_placeholder = st.empty()
+        
         # Send user's question to our chain
         result = st.session_state['chain']({"question": query})
         response = result['answer']
@@ -59,9 +78,74 @@ if query := st.chat_input("Let's chat"):
         for chunk in response.split():
             full_response += chunk + " "
             time.sleep(0.05)
+            
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
+        
         message_placeholder.markdown(full_response)
+        
+        modal = Modal(title="", key="modal")
+        
+        with modal.container():
+
+            user_input = st.session_state.messages[-1]["content"]
+            model_output = full_response
+            st.write(user_input)
+            st.write(model_output)
+    
+            with st.form("feedback"):
+
+                # Initialize disabled for form_submit_button to False
+                if "disabled" not in st.session_state:
+                    st.session_state.disabled = False
+        
+                q1 = st.radio(
+                    label="Rate how accurately the chatbot answered your question.",
+                    options=["Very Poor", "Poor", "Acceptable", "Good", "Very Good"],
+                    index=2,
+                    horizontal=True
+                )
+                q2 = st.radio(
+                    label="Rate the level that the agent’s response enabled you to learn based on your personal experience.",
+                    options=["Very Poor", "Poor", "Acceptable", "Good", "Very Good"],
+                    index=2,
+                    horizontal=True
+                )
+                q3 = st.radio(
+                    label="Rate your level of understanding of the agent’s response.",
+                    options=["Very Poor", "Poor", "Acceptable", "Good", "Very Good"],
+                    index=2,
+                    horizontal=True
+                )
+                q4 = st.radio(
+                    label="If there are examples present, rate your level of understanding of the examples.",
+                    options=["Very Poor", "Poor", "Acceptable", "Good", "Very Good", "N/A - Not Applicable"],
+                    index=2,
+                    horizontal=True
+                )
+
+                submitted = st.form_submit_button("Submit")
+                
+                user_input = user_input.replace("'", "").replace('"', "").replace(",", "\,").replace("\n", " ")
+                model_output = model_output.replace("'", "").replace('"', "").replace(",", "\,").replace("\n", " ")
+                
+                timestamp = datetime.datetime.now(tz=pytz.timezone('America/New_York')).strftime("%Y-%m-%d %H:%M:%S")
+
+                con = sqlite3.connect("results.db")
+                with con:
+                    cur = con.cursor()
+                    cur.execute(f"""
+                            INSERT INTO results
+                            VALUES
+                            ('{user_id}', 
+                            '{user_input}', 
+                            '{model_output}', 
+                            '{q1}', 
+                            '{q2}', 
+                            '{q3}', 
+                            '{q4}',
+                            '{timestamp}')
+                        """)
 
     # Add assistant message to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
